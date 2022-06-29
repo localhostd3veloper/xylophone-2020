776051b9f410cb6aedc4a88d2e3cc835f039c3a1
localhostd3veloper <gautamanand022@gmail.com>
2022-06-29 19:09:59 +0530

    Added Sounds

--- lib/main.dart
+++ lib/main.dart
@@ -8,21 +8,62 @@ import 'package:flutter/material.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNumber) {
+    AssetsAudioPlayer audioPlayer = AssetsAudioPlayer();
+    audioPlayer.open(Audio('assets/note$soundNumber.wav'));
+  }
+
   @override
   Widget build(BuildContext context) {
+    // V I B G Y O R
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(
-            child: Center(
-              child: TextButton(
+          child: Column(
+            children: [
+              MaterialButton(
+                color: Colors.red[400],
                 onPressed: () {
-                  AssetsAudioPlayer audioPlayer = AssetsAudioPlayer();
-                  audioPlayer.open(Audio('assets/note1.wav'));
+                  playSound(1);
                 },
-                child: const Text('Note 1'),
               ),
-            ),
+              MaterialButton(
+                color: Colors.orange[400],
+                onPressed: () {
+                  playSound(2);
+                },
+              ),
+              MaterialButton(
+                color: Colors.yellow[400],
+                onPressed: () {
+                  playSound(3);
+                },
+              ),
+              MaterialButton(
+                color: Colors.green[400],
+                onPressed: () {
+                  playSound(4);
+                },
+              ),
+              MaterialButton(
+                color: Colors.blue[400],
+                onPressed: () {
+                  playSound(5);
+                },
+              ),
+              MaterialButton(
+                color: Colors.lightBlue[400],
+                onPressed: () {
+                  playSound(6);
+                },
+              ),
+              MaterialButton(
+                color: Colors.purple[400],
+                onPressed: () {
+                  playSound(7);
+                },
+              ),
+            ],
           ),
         ),
       ),
